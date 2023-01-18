3ff643b1e0cf98b3ffe1f59d1db32e3e68c69ce9
Petru4e <Petru4e85@mail.ru>
2023-01-18 20:47:03 +0300

    Исправлено добавление новой заявки

--- CarControl/Auto_Add.xaml.cs
+++ CarControl/Auto_Add.xaml.cs
@@ -28,35 +28,75 @@ namespace CarControl
         }
         private void Add_Btn_Click(object sender, RoutedEventArgs e)
         {
-            string number = Number.Text.Trim().ToLower();
-            string model = Model.Text.Trim().ToLower();
+            string number = Number.Text.Replace(" ", "").ToUpper();
+            string model = Model.Text;
             string comment = Comment.Text;
             string start_date = Start_Date.Text;
             string end_date = End_Date.Text;
-            Regex regex = new Regex(@"/^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui");
-            MatchCollection matches = regex.Matches(number);
-            if (matches.Count > 0)
+            //Regex regex = new Regex(@"/^[АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{2}\d{2,3}$/ui");
+            //MatchCollection matches = regex.Matches(number);
+
+            if (number.Length == 0)
             {
-                Number.ToolTip = "";
-                Number.Background = Brushes.Transparent;
-              
+                Number.ToolTip = "Введите номер";
+                Number.Background = Brushes.Red;
+            }
+            else if (model.Length == 0)
+            {
+                Model.ToolTip = "Введите марку автомобиля";
+                Model.Background = Brushes.Red;
+            }
+            else if (comment.Length == 0)
+            {
+                Comment.ToolTip = "Введите коментарий: откуда заявка, кто и когда сформировал";
+                Comment.Background = Brushes.Red;
+            }
+            else if (start_date.Length == 0)
+            {
+                Start_Date.ToolTip = "Введите дату начала действия заявки";
+                Start_Date.Background = Brushes.Red;
+            }
+            else if (end_date.Length == 0)
+            {
+                End_Date.ToolTip = "Введите дату окончания действия заявки";
+                End_Date.Background = Brushes.Red;
+            }
+            else if (Regex.IsMatch(number, @"\p{IsCyrillic}"))
+            {
+                Number.ToolTip = "Введите номер используя английскую раскладку клавиатуры";
+                Number.Background = Brushes.Red;
             }
             else
             {
-                Number.ToolTip = "Не корректный номер";
-                Number.Background = Brushes.Red;
+                Number.ToolTip = "";
+                Number.Background = Brushes.Transparent;
+                Model.ToolTip = "";
+                Model.Background = Brushes.Transparent;
+                Comment.ToolTip = "";
+                Comment.Background = Brushes.Transparent;
+                Start_Date.ToolTip = "";
+                Start_Date.Background = Brushes.Transparent;
+                End_Date.ToolTip = "";
+                End_Date.Background = Brushes.Transparent;
             }
 
-            
-            Car car = new Car(number, model, comment, start_date, end_date);
+            if((string)Number.ToolTip == "")
+            {
+                Car car = new Car(number, model, comment, start_date, end_date);
 
-            db.Cars.Add(car);
-            db.SaveChanges();
+                db.Cars.Add(car);
+                db.SaveChanges();
+
+                MessageBox.Show("Добавлено");
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                Hide();
+            }
+            else
+            {
+                MessageBox.Show($"Исправьте замечания");
+            }
             
-            MessageBox.Show("Добавлено");
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            Hide();
         }
     }
 }
